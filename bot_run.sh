6d2603a3f202b6420b8026199b7b968276c5b0d5
alekz_gg <alekz.gavrilov@gmail.com>
2022-02-07 22:58:20 +0300

    change_lib_aiogram

---
+++ bot_run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+@echo off
+
+call %~dp0venv\activate
+
+set TOKEN=5264948128:AAEDHAcbIvCXcOENgFTJZaLEOin53wyZWQw
+
+python main.py
+
+pause
--- main.py
+++ main.py
@@ -1,73 +1,149 @@
-import telebot
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
-token = "5264948128:AAEDHAcbIvCXcOENgFTJZaLEOin53wyZWQw"
+import os
 
-bot = telebot.TeleBot(token)
+TOKEN="5264948128:AAEDHAcbIvCXcOENgFTJZaLEOin53wyZWQw"
 
-start = """
+bot = Bot(TOKEN)
+dp = Dispatcher(bot)
+
+HELP = """
 /start - показать справку
-/shoksha - выбрать шокшу
-/poksha - выбрать покшу
-/who_is_shoksha - узнать кто шокша
-/who_is_poksha - узнать кто покша"""
+"""
 
-# /poksha
-# /who_is_poksha - узнать кто покша в этом чате сегодня
-# /rating - кто есть кто
+@dp.message_handler(commands=['start'])
+async def help(message: types.Message):
+    # await message.answer(message.text)
+    # await message.reply(message.text)
+    await bot.send_message(message.from_user.id, HELP)
 
-Shoksha = ''
-Poksha = ''
-
-def get_rate(message, task):
-    pass
-
-@bot.message_handler(commands=["start"])
-def help(message):
-    bot.send_message(message.chat.id, start)
-
-# про шокшу
-@bot.message_handler(commands=["shoksha"])
-def shoksha(message):
-    global Shoksha
-    Shoksha = message.from_user.username
-    text = ' теперь ты, @' + Shoksha + ', шокша'
-    bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=["who_is_shoksha"])
-def who_is_shoksha(message):
-    if Shoksha:
-        text = '@' + Shoksha + ' шокша'
-        bot.send_message(message.chat.id, text)
-    else:
-        text = "Шокша еще не определён"
-        bot.send_message(message.chat.id, text)
-
-# про покшу
-@bot.message_handler(commands=["poksha"])
-def poksha(message):
-    global Poksha
-    Poksha = message.from_user.username
-    text = ' теперь @' + Poksha + ' покша'
-    bot.send_message(message.chat.id, text)
-
-@bot.message_handler(commands=["who_is_poksha"])
-def who_is_poksha(message):
-    if Poksha:
-        text = '@' + Poksha + ' покша'
-        bot.send_message(message.chat.id, text)
-    else:
-        text = "Покша еще не определён"
-        bot.send_message(message.chat.id, text)
+executor.start_polling(dp, skip_updates=True)
 
 
-@bot.message_handler(commands=["show"])
-def show(message):
-    pass
 
-# Постоянно обращается к серверу телеграмма
-bot.polling(none_stop=True)
 
-def choose(commands=[]):
-    pass
 
-# test_git_commit
+
+
+# start = """
+# /start - показать справку
+# /shoksha - выбрать шокшу
+# /poksha - выбрать покшу
+# /who_is_shoksha - узнать кто шокша
+# /who_is_poksha - узнать кто покша"""
+
+# # /poksha
+# # /who_is_poksha - узнать кто покша в этом чате сегодня
+# # /rating - кто есть кто
+
+# Shoksha = ''
+# Poksha = ''
+
+# def get_rate(message, task):
+#     pass
+
+# @bot.message_handler(commands=["start"])
+# def help(message):
+#     bot.send_message(message.chat.id, start)
+
+# # про шокшу
+# @bot.message_handler(commands=["shoksha"])
+# def shoksha(message):
+#     global Shoksha
+#     Shoksha = message.from_user.username
+#     text = ' теперь ты, @' + Shoksha + ', шокша'
+#     bot.send_message(message.chat.id, text)
+
+# @bot.message_handler(commands=["who_is_shoksha"])
+# def who_is_shoksha(message):
+#     if Shoksha:
+#         text = '@' + Shoksha + ' шокша'
+#         bot.send_message(message.chat.id, text)
+#     else:
+#         text = "Шокша еще не определён"
+#         bot.send_message(message.chat.id, text)
+
+# # про покшу
+# @bot.message_handler(commands=["poksha"])
+# def poksha(message):
+#     global Poksha
+#     Poksha = message.from_user.username
+#     text = ' теперь @' + Poksha + ' покша'
+#     bot.send_message(message.chat.id, text)
+
+# @bot.message_handler(commands=["who_is_poksha"])
+# def who_is_poksha(message):
+#     if Poksha:
+#         text = '@' + Poksha + ' покша'
+#         bot.send_message(message.chat.id, text)
+#     else:
+#         text = "Покша еще не определён"
+#         bot.send_message(start = """
+# /start - показать справку
+# /shoksha - выбрать шокшу
+# /poksha - выбрать покшу
+# /who_is_shoksha - узнать кто шокша
+# /who_is_poksha - узнать кто покша"""
+
+# # /poksha
+# # /who_is_poksha - узнать кто покша в этом чате сегодня
+# # /rating - кто есть кто
+
+# Shoksha = ''
+# Poksha = ''
+
+# def get_rate(message, task):
+#     pass
+
+# @bot.message_handler(commands=["start"])
+# def help(message):
+#     bot.send_message(message.chat.id, start)
+
+# # про шокшу
+# @bot.message_handler(commands=["shoksha"])
+# def shoksha(message):
+#     global Shoksha
+#     Shoksha = message.from_user.username
+#     text = ' теперь ты, @' + Shoksha + ', шокша'
+#     bot.send_message(message.chat.id, text)
+
+# @bot.message_handler(commands=["who_is_shoksha"])
+# def who_is_shoksha(message):
+#     if Shoksha:
+#         text = '@' + Shoksha + ' шокша'
+#         bot.send_message(message.chat.id, text)
+#     else:
+#         text = "Шокша еще не определён"
+#         bot.send_message(message.chat.id, text)
+
+# # про покшу
+# @bot.message_handler(commands=["poksha"])
+# def poksha(message):
+#     global Poksha
+#     Poksha = message.from_user.username
+#     text = ' теперь @' + Poksha + ' покша'
+#     bot.send_message(message.chat.id, text)
+
+# @bot.message_handler(commands=["who_is_poksha"])
+# def who_is_poksha(message):
+#     if Poksha:
+#         text = '@' + Poksha + ' покша'
+#         bot.send_message(message.chat.id, text)
+#     else:
+#         text = "Покша еще не определён"
+#         bot.send_message(message.chat.id, text)
+
+
+# @bot.message_handler(commands=["show"])
+# def show(message):
+#     pass
+
+# # Постоянно обращается к серверу телеграмма
+# bot.polling(none_stop=True)
+
+# def choose(commands=[]):
+#     pass
+
+# # test_git_commit
