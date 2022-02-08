ed2bf0242cc38bfffe7502c34eed09f27a5e666e
alekz_gg <alekz.gavrilov@gmail.com>
2022-02-08 23:42:28 +0300

    release 2

--- main.py
+++ main.py
@@ -1,23 +1,57 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+import time
 
-import os
 
 TOKEN="5264948128:AAEDHAcbIvCXcOENgFTJZaLEOin53wyZWQw"
 
 bot = Bot(TOKEN)
 dp = Dispatcher(bot)
 
+"""========================CLIENT========================"""
+
+@dp.message_handler(commands=['start', 'help'])
+async def command_start(message: types.Message):
+    try:
+        await bot.send_message(message.from_user.id, "Здравствуй, мой юный друг", )
+        await message.delete()
+    except:
+        await message.reply("Общение с ботом через ЛС, напишите ему :\nhttps://t.me/wtf_repeat_bot")
+
+@dp.message_handler(commands=['mojet_bahnem'])
+async def command_start(message: types.Message):
+    await message.reply("Обязательно бахнем")
+    time.sleep(2)
+    await bot.send_message(message.from_user.id, "И не раз")
+    time.sleep(2)
+    await bot.send_message(message.from_user.id, "Весь мир в труху")
+    # await bot.send_message(message.from_user.id, "но через ", time_to_next_party())
+
+"""========================ADMIN========================"""
+
+async def next_party():
+    pass
+
+async def time_to_next_party():
+    pass
+
+async def get_friday(ctime):
+    pass
+
+"""========================GENERAL========================"""
+
 HELP = """
 /start - показать справку
+/mojet_bahnem
 """
 
-@dp.message_handler(commands=['start'])
+@dp.message_handler()
 async def help(message: types.Message):
-    # await message.answer(message.text)
-    # await message.reply(message.text)
-    await bot.send_message(message.from_user.id, HELP)
+    if message.text.lower() == "Привет".lower():
+        # await message.answer("Ну здарова")
+        # await message.reply(message.text)
+        await bot.send_message(message.from_user.id, HELP)
 
 executor.start_polling(dp, skip_updates=True)
 
